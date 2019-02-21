2b51cfccc756484cc79e62ad6de4b31adeba2774
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-02-21 20:09:04 +0000

    Integrating Netlify Form

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,6 +100,7 @@ class HomeIndex extends React.Component {
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form name="contact" method="POST" data-netlify="true">
+                                    <input type="hidden" name="form-name" value="contact" />
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
