124fc374757338ad44bec21e6aa7ded6dbbb2c16
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-02-21 16:45:58 +0000

    Added Social Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,8 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://github.com/aldmarj" className="icon fa-github" target="_blank"><span className="label">Github</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/aldmar-joubert-089488bb/" className="icon fa-linkedin" target="_blank"><span className="label">LinkedIn</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Aldmar Joubert 2019</li>
