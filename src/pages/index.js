858f20e8364d370f54bd8e44a0f4de0037068f6c
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-02-21 14:47:24 +0000

    Spelling Corretion and removed Learn More button

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,9 +92,6 @@ class HomeIndex extends React.Component {
                             <h2>About Me</h2>
                         </header>
                         <p>I am a recent computer science graduate, who is currently pursuing educational and professional opportunities to find my niche in this ever-growing industry. My current focus is web development. recently I have been exploring the capabilities of the JAM stack. The possibilities excite me due to its flexibility, extensibility and its reach. I seek out opportunities that allow me to harness the power of microservices that can come together on a web application and solve real world problems.</p>
-                        <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
                     </section>
 
                     <section id="two">
