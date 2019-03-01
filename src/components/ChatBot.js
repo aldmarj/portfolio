e9f4b1cde9f545d0881d12918f7ff5feb2bee33d
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2019-03-01 17:24:37 +0000

    Made the link to the local server for chatbot

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -1,83 +1,50 @@
-// import React, { Component } from 'react';
-// import  Widget, { addResponseMessage } from 'react-chat-widget';
-// import Pusher from 'pusher-js';
-
-// import 'react-chat-widget/lib/styles.css';
-
-// class ChatBot extends Component {
- 
-//   componentDidMount() {
-//         addResponseMessage("Welcome to this awesome chat!");
-        
-//         const myPusher = new Pusher('1c026c5113825e043b38', {
-//             cluster: 'eu',
-//             encrypted: true,
-//         });
-
-//         const channel = myPusher.subscribe('bot');
-//         channel.bind('bot-response', data => {
-        
-//         addResponseMessage(data.message);
-        
-//     });
-//   }
-
-//   handleSubmit(newMessage)  {
-
-//     fetch('http://localhost:5000/chat', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json'},
-//       body: JSON.stringify({
-//         message: newMessage,
-//       }),
-//     });
-//   };
-
-//   handleNewUserMessage = (newMessage) => {
-//     this.handleSubmit(newMessage);
-//   }
-
-//   render(){
-//     return(
-//       <Widget 
-//         handleNewUserMessage={this.handleNewUserMessage}
-//         title="Weather ChatBot"
-//         subtitle="Ask me about the weather in your area." 
-//       />
-//     )
-//   }
-
-// }
-
-// export default ChatBot
-
 import React, {Component} from 'react'
+import Pusher from 'pusher-js';
 import {Launcher} from 'react-chat-window'
-import messageHistory from './messageHistory'
 
 class ChatBot extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      messageList: messageHistory
+      messageList: []
     };
+  }
+
+  componentDidMount() {
+    const pusher = new Pusher('1c026c5113825e043b38', {
+      cluster: 'eu',
+      encrypted: true,
+    });
+
+    const channel = pusher.subscribe('bot');
+    channel.bind('bot-response', data => {
+      this._sendMessage(data.message);
+    });
   }
 
   _onMessageWasSent(message) {
     this.setState({
       messageList: [...this.state.messageList, message]
     })
+
+    fetch('http://localhost:5000/chat', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        message: message.data.text,
+      }),
+    });
   }
 
   _sendMessage(text) {
     if (text.length > 0) {
       this.setState({
         messageList: [...this.state.messageList, {
-          author: 'them',
+          author: 'ai',
           type: 'text',
-          data: { text }
+          data: { text } 
         }]
-      })
+      }) 
     }
   }
 
@@ -85,12 +52,10 @@ class ChatBot extends Component {
     return (<div>
       <Launcher
         agentProfile={{
-          teamName: 'react-chat-window',
-          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+          teamName: 'react-chat-window'
         }}
         onMessageWasSent={this._onMessageWasSent.bind(this)}
         messageList={this.state.messageList}
-        showEmoji
       />
     </div>)
   }
